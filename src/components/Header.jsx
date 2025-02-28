51ed3f957d3c4efecf25d1b09703f0c76fea1bce
S Hegde <shreyas567880@gmail.com>
2025-02-27 23:24:10 -0500

    Created a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import '../styles/header.css'
+
+export default function Header() {
+    return(
+        <header className='header'>
+            <img src='/images/Globe.png' alt="Globe" className='header-img' />
+            <h1 className='header-title'>t-dot travel journal.</h1>
+        </header>
+    )
+}
