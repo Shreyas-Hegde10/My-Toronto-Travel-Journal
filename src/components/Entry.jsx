6c566d66c1577d71072eae808220b30ea90af0e6
S Hegde <shreyas567880@gmail.com>
2025-03-09 00:06:59 -0500

    Passed and read props using map; added two more entries

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,3 +1,5 @@
+import '../styles/entry.css'
+
 function Entry(props) {
     return(
         <article className="journal-entry">
@@ -5,8 +7,9 @@ function Entry(props) {
                 <img className="main-img" src={props.img.src} alt={props.img.alt}/> 
             </div> 
             <div className="info-container">
+                <img className='address-marker' src="/images/Fill 219.png" alt="marker" />
                 <span className="address">{props.address}</span> 
-                <a href={props.googleMapsLink} target="_blank"></a>
+                <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
                 <h2 className="entry-title">{props.title}</h2>  
                 <p className="entry-text">{props.text}</p>
             </div>
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+   {
+    id: 1, 
+    img: {
+        src: "/images/3b48931eb9566083a0c7ed9c98daa37c (1).png", 
+        alt: "CN Tower"
+    }, 
+    address: "290 Bremner Blvd,Toronto",  
+    googleMapsLink : "https://www.google.com/maps/place/CN+Tower/@43.6425701,-79.3896317,17z/data=!3m2!4b1!5s0x882b34d819a55ff7:0xad7cf7bcaf4e239b!4m6!3m5!1s0x882b34d68bf33a9b:0x15edd8c4de1c7581!8m2!3d43.6425662!4d-79.3870568!16zL20vMDF0d3M?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D", 
+    title: "CN Tower",
+    text: "The CN Tower is a 553.3 m-high communications and observation tower in Toronto. Its name CN refers to Canadian National, the railway company that built the tower in 1976. It held the record for the world's tallest free-standing structure from 1975 until 2007, when it was surpassed by the Burj Khalifa. It is currently the 10th tallest free-standing structure in the world. It houses several observation decks, a revolving restaurant at 350m, and an entertainment complex. Tickets range from $29.00 for children to $56.00 for adults."
+   }, 
+
+   {
+    id: 2, 
+    img: {
+        src: "/images/101838393_10158309896514144_1312539256878530560_n (1) (1).png",
+        alt: "Art Gallery of Ontario"
+    }, 
+    address: "317 Dundas St W,Toronto",  
+    googleMapsLink : "https://www.google.com/maps/place/Art+Gallery+of+Ontario/@43.6536105,-79.3950872,17z/data=!3m1!4b1!4m6!3m5!1s0x882b34c5ed5319bd:0xda277755ede046af!8m2!3d43.6536066!4d-79.3925123!16zL20vMDJ2eWxy?entry=ttu&g_ep=EgoyMDI1MDMwNC4wIKXMDSoASAFQAw%3D%3D", 
+    title: "Art Gallery of Ontario",
+    text: "The Art Gallery of Ontario(aka AGO) is an art gallery in Toronto which is located in the Grange Park neighbourhood of Downtown Toronto. It takes up about 480,000 sqft of physical space and houses over 120,000 works spanning from the first century to the present day. Tickets are free for all Ontarians under the age of 25."
+   }, 
+
+   {
+    id: 3, 
+    img: {
+        src: "/images/depositphotos_189455642_ds_700-full (1).png",
+        alt: "Casa Loma"
+    }, 
+    address: "1 Austin Terrace,Toronto",  
+    googleMapsLink : "https://www.google.com/maps/place/Casa+Loma/@43.6823613,-79.4185359,15z/data=!4m10!1m2!2m1!1scasa+loma!3m6!1s0x882b349dcf25a1b3:0x617cc8c102d6584f!8m2!3d43.6780371!4d-79.4094439!15sCgljYXNhIGxvbWFaCyIJY2FzYSBsb21hkgEGbXVzZXVt4AEA!16zL20vMDFfenQ0?entry=ttu&g_ep=EgoyMDI1MDMwNC4wIKXMDSoASAFQAw%3D%3D", 
+    title: "Casa Loma",
+    text: "The Casa Loma is a Gothic Revival mansion and garden located in the Casa Loma neighbourhood in Midtown Toronto. It was constructed from 1911 to 1914 as a residence for financier Sir Henry Pellatt. Currently, it operates as a mueseum as well as a venue for holding events and shooting movies and TV shows. Tickets range from $25 for children to $40 for adults."
+   }
+   
+]
