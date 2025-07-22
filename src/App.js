b8cd68b8a1c5544d983ffcd08cf2d245a2778cb3
S Hegde <shreyas567880@gmail.com>
2025-07-22 17:03:40 -0400

    Added sliding functionality using arrows

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Header from "./components/Header"
 import Slider from "./components/Slider";
-import data from "./data";
+import {React, useState } from "react"; 
+import data from './data';
+
 
 
 function App() { 
 
+  const[currentIndex, setCurrentIndex] = useState(0); 
+
+  const previousSlide = () => {
+    setTimeout(() => {const newIndex = currentIndex === 0 ? data.length - 1 : currentIndex - 1; 
+      setCurrentIndex(newIndex);}, 300)
+      } 
+
+  const nextSlide = () => {
+    setTimeout(() => {const newIndex = currentIndex === data.length - 1 ? 0 : currentIndex +1; 
+      setCurrentIndex(newIndex);}, 300)  
+      }
+
   return ( 
     <>
       <Header />
-      <Slider />
+      <Slider currentIndex = {currentIndex} previousSlide = {previousSlide} nextSlide = {nextSlide} />
     </>
   );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,19 @@
 import '../styles/slider.css' 
-import data from '../data';
+import data from '../data'; 
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
 
-function Slider() {
+function Slider(props) {
 
     return(
         <div className='slider'>
-            <div className='slider__image' style={{ backgroundImage: `url(${data[0].url})`}}></div>
+            <div className='slider__image' style={{ backgroundImage: `url(${data[props.currentIndex].url})`}}>
+                <div className='slider__left_arrow'>
+                <BsChevronCompactLeft size={30} onClick={props.previousSlide} /> 
+                </div>  
+                <div className='slider__right_arrow'>
+                    <BsChevronCompactRight size={30} onClick={props.nextSlide} /> 
+                </div> 
+            </div> 
         </div>
     )
 }
