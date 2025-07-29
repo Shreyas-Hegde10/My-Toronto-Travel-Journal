63ce74f18060cae0e7d7c4b58d4240bbc6a0b814
S Hegde <shreyas567880@gmail.com>
2025-07-29 01:46:15 -0400

    Added a typewriter effect to the snippet text as well as some additional styling

--- src/components/Snippet.jsx
+++ src/components/Snippet.jsx
@@ -1,11 +1,22 @@
 import '../styles/snippet.css';
+import Typewriter from 'typewriter-effect'; 
 
 function Snippet(props){
     return(
         <div className="snippet">
             <h2 className="snippet__title">Snippets from My Journal</h2> 
             <div className={`snippet__content ${props.isFading ? 'fade': null}`}>
-                <p className="snippet__text">{props.snippet}</p>
+                <p className="snippet__text">
+                    <Typewriter
+                        options={{
+                            strings: props.snippet,
+                            autoStart: true,
+                            loop: false,
+                            delay: 50, 
+                            cursor: '|',
+                        }}
+                    />
+                </p>
             </div>
         </div>
     )
